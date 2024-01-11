c488022c3fc996b857b8d550168f3b4f7d52e9e8
devlrakib <devrakib5@gmail.com>
2024-01-11 18:53:45 +0600

    first commit

---
+++ index1.js
@@ -0,0 +1,6 @@
+// ("" or, ''  double quation ba single quation er moddhe string type likha hoi)
+// Here Printing Details
+document.write("This Is String Data Type<br>") // string Data Type
+document.write("This Is Number Data Type "+30+'<br>')// number data type
+document.write("Bangladesh")
+document.write(True) // Bolean Data Type 
